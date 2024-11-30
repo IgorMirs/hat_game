154f426c02db9ced47003ae243abea060bd05133
Igor Mirsalikhov <igor_air@MacBookAir.lan>
2024-11-30 09:13:40 -0500

    Update GameState class

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -1,7 +1,12 @@
 #include "GameState.h"
+#include "Game.h"
 
 GameState::GameState(Game* game)
     :m_game(game) 
+{
+}
+
+GameState::~GameState()
 {
 }
 
@@ -23,4 +28,12 @@ void GetTeams::update(sf::Time delta) {
     
 void GetTeams::draw(sf::RenderWindow& window) {
 
+}
+
+
+void GameState::configureText(sf::Text& text) {
+    text.setCharacterSize(50);
+    text.setFont(m_game->getFont());
+    text.setFillColor(sf::Color::White);
+    text.setStyle(sf::Text::Bold);
 }
--- src/GameState.h
+++ src/GameState.h
@@ -8,20 +8,30 @@ class Game;
 class GameState {
 public:
     GameState(Game* game);
+    virtual ~GameState();
     Game* getGame() const;
+    
     enum State {
+        START,
         GET_TEAMS,
-        START
+        COUNT
     };
+
+    void configureText(sf::Text& text);
 
     virtual void handle_input() = 0;
     virtual void update(sf::Time delta) = 0;
     virtual void draw(sf::RenderWindow& window) = 0;
 
-
 private:
     Game* m_game;
 };
+
+
+
+
+
+
 
 class GetTeams : public GameState {
 public:
@@ -31,6 +41,7 @@ public:
     void update(sf::Time delta);
     void draw(sf::RenderWindow& window);
 };
+
 
 
 
