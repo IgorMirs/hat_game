399974b3ff118deb7035fc0d58b822942749906f
Igor Mirsalikhov <igor_air@MacBookAir.lan>
2024-12-01 08:54:31 -0500

    Optimize Game class. Utilize game states.

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,20 +2,13 @@
 #include <iostream>
 #include <exception>
 
-Game::Game() :  m_window({1920u, 1080u}, "Hat Game"), m_status(NO_INPUT), 
-                input_msg(""), numberOfTeams(0) 
+Game::Game() :  m_window({1920u, 1080u}, "Hat Game"), numberOfTeams(0) 
 {
     m_window.setFramerateLimit(144);
     loadFont(m_font);
-    m_msg.setString("");
-    m_input.setString("");
-    setText(m_msg);
-    setText(m_input);
-    m_input.setPosition(40, 55);
-
     //init game states
     m_gameStates[GameState::State::START] = new StartState(this); 
-    m_gameStates[GameState::State::GET_TEAMS] = new GetTeams(this); 
+    m_gameStates[GameState::State::GET_TEAMS] = new GetTeamsState(this); 
 
     changeGameState(GameState::START);
 }
@@ -30,44 +23,22 @@ Game::~Game() {
 sf::Font& Game::getFont() {
     return m_font;
 }
-sf::Text& Game::getPrompt() {
-    return m_msg;
-}
-
-sf::Text& Game::getInputText() {
-    return m_input;
-}
-
 
 
 void Game::run() {
-    std::cout << "HERE\n";
     while (m_window.isOpen())
     {
         for (auto event = sf::Event(); m_window.pollEvent(event);) {
             if (event.type == sf::Event::Closed) {
                 m_window.close();
             }
-            // else if (event.type == sf::Event::TextEntered) {
-            //     switch (m_status) {
-            //     case TEAMS_NUMBER_ENTERED:
-            //         m_msg.setString("Enter the word\n> ");
-            //         break;
-            //     case NO_INPUT:
-            //         m_msg.setString("Enter the number of teams\n> ");
-            //         //create_intro(event);
-            //         break;
-                
-            //     default:
-            //         break;
-            //     }
-            // }
+            if (event.type == sf::Event::TextEntered || event.type == sf::Event::KeyPressed) {
+                m_currentState->handle_input(event);
+            }
         }
         m_currentState->update(sf::seconds(1));
         m_window.clear();
         m_currentState->draw(m_window);
-        //m_window.draw(m_msg);
-        //m_window.draw(m_input);
         m_window.display();
     }
 }
@@ -79,20 +50,6 @@ void Game::setText(sf::Text& text) {
     text.setStyle(sf::Text::Bold);
 }
 
-void Game::getMsg() {
-    switch (m_status) {
-    case TEAMS_NUMBER_ENTERED:
-        m_msg.setString("Enter the word\n> ");
-        break;
-    case NO_INPUT:
-        m_msg.setString("Enter the number of teams\n> ");
-        break;
-    
-    default:
-        break;
-    }
-}
-
 void Game::loadFont(sf::Font& font) {
     if (!font.loadFromFile("/Users/igor_air/Documents/coding/01_hat_game/fonts/arial.ttf")) {
         throw std::runtime_error("ERROR loading font!");
@@ -100,20 +57,17 @@ void Game::loadFont(sf::Font& font) {
     }
 }
 
-void Game::create_intro(sf::Event& event) {
-    if (event.text.unicode > '1' && event.text.unicode <= '9') {
-        input_msg.clear();
-        input_msg += event.text.unicode;
-        m_input.setString(input_msg);
-    }
-    else if (!m_input.getString().isEmpty() && event.text.unicode == '\n') { //number of teams is entered
-        input_msg = m_input.getString();
-        numberOfTeams = std::stoi(input_msg);
-        m_status = TEAMS_NUMBER_ENTERED;
-        m_input.setString("");
-    }
-}
-
 void Game::changeGameState(GameState::State gameState) {
     m_currentState = m_gameStates[gameState];
+}
+
+
+unsigned Game::getWindowWidth() const {
+    sf::Vector2u v = m_window.getSize();
+    return v.x;
+}
+
+unsigned Game::getWindowHeight() const {
+    sf::Vector2u v = m_window.getSize();
+    return v.y;
 }
--- src/Game.h
+++ src/Game.h
@@ -2,16 +2,10 @@
 #define GAME_H
 #include "GameState.h"
 #include "StartState.h"
+#include "GetTeamsState.h"
 #include "SFML/Graphics.hpp"
 #include <string>
 #include <array>
-
-
-enum Status {
-    TEAMS_NUMBER_ENTERED,
-    WORDS_ENTERED,
-    NO_INPUT
-};
 
 class Game {
 public:
@@ -22,8 +16,9 @@ public:
     void setText(sf::Text& text);
 
     sf::Font& getFont();
-    sf::Text& getPrompt();
-    sf::Text& getInputText();
+    
+    unsigned getWindowWidth() const;
+    unsigned getWindowHeight() const;
 
     void changeGameState(GameState::State gameState);
 
@@ -32,16 +27,9 @@ private:
     GameState* m_currentState;
     std::array<GameState*, GameState::State::COUNT> m_gameStates;
     sf::Font m_font;
-    sf::Text m_msg, m_input;
-    enum Status m_status;
-    std::string input_msg;
-
-    int numberOfTeams;
-
     void loadFont(sf::Font& font);
-    void getMsg();
 
-    void create_intro(sf::Event& event);
+    int numberOfTeams; //TODO remove
 };
 
 #endif //GAME_H
